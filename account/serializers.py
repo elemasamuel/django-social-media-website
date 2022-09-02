3dcf24d6d5d3cfe694481fcd41cdab1340723658
elemasamuel <99813035+elemasamuel@users.noreply.github.com>
2022-09-02 02:37:43 +0200

    Update serializers.py

--- account/serializers.py
+++ account/serializers.py
@@ -1,16 +1,25 @@
+from cgitb import lookup
 from rest_framework import serializers
 
 from django.contrib.auth.models import User
 
 from account.models import Profile
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
-class HyperUserSerializer(serializers.HyperlinkedModelSerializer):
+class HyperUserSerializer(serializers.ModelSerializer):
+    favorites = serializers.HyperlinkedRelatedField(
+        read_only=True,
+        many=True,
+        view_name='postcomment',
+        lookup_field='id',
+        lookup_url_kwarg='post_id')
     class Meta:
         model = Profile
         fields = '__all__'
         depth = 1
+
