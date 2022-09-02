39f4557ccf10f83854ebd8a74691bb48d1b998d6
elemasamuel <99813035+elemasamuel@users.noreply.github.com>
2022-09-02 02:52:54 +0200

    Update models.py

--- account/models.py
+++ account/models.py
@@ -51,15 +51,7 @@ class Profile(models.Model):
             pic.thumbnail(SIZE, Image.LANCZOS)
             pic.save(self.picture.path)
 
-def create_user_profile(sender, instance, created, **kwargs):
-	if created:
-		Profile.objects.create(user=instance)
 
-def save_user_profile(sender, instance, **kwargs):
-	instance.profile.save()
-
-post_save.connect(create_user_profile, sender=User)
-post_save.connect(save_user_profile, sender=User)
 
 
 
